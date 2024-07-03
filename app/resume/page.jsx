46c1977a133e20a521f9c736395e2d79001d4a30
ignacio <ignaciocarrabs@gmail.com>
2024-07-03 20:35:09 -0300

    educacion

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -8,7 +8,7 @@ import {
   FaReact,
   FaFigma,
   FaNodeJs,
-  FaJava
+  FaJava,
 } from "react-icons/fa";
 import {
   SiTailwindcss,
@@ -25,9 +25,11 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { BsArrowDownRight } from "react-icons/bs";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const about = {
   title: "About me",
@@ -63,17 +65,18 @@ const about = {
 
 const education = {
   icon: "/assets/resume/cap.svg",
-  title: "",
-  desc: "Lorem ipsum dolor sit amet conectetur adipisicng elit. Voluptuates uqiakjdasm sunt epxlica vinetro.",
+  title: "Educación Profesional",
+  desc: "Cumplí con 1568 horas de cursado teórico-práctico para que me otorguen la certificación profesional como desarrollador, en mi paso por la Facultad de Arquitectura y Urbanismo adquirí habilidades blandas y conocimientos sólidos en el área del diseño.  ",
   items: [
     {
-      institución: "Digital House | MercadoLibre & Globant",
-      position: "Certified Tech Developer",
+      institución: "Digital House | MercadoLibre & Globant.",
+      position: "Certified Tech Developer.",
       duration: "2021 - 2023",
+      certi: "Ver certificación",
     },
     {
-      institución: "Universidad Nacional del Litoral",
-      position: "Arquitectura, diseño y urbanismo",
+      institución: "Universidad Nacional del Litoral.",
+      position: "Arquitectura, diseño y urbanismo.",
       duration: "2018 - Sin finalizar.",
     },
   ],
@@ -152,16 +155,65 @@ const Resume = () => {
           className="flex flex-col xl:flex-row gap-[60px]"
         >
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
-            <TabsTrigger value="educación">Education</TabsTrigger>
+            <TabsTrigger value="educación">Educación</TabsTrigger>
             <TabsTrigger value="habilidades">Mis habilidades</TabsTrigger>
-            <TabsTrigger value="sobremi">Sobre mi</TabsTrigger>
+            <TabsTrigger value="sobremi">Sobre mí</TabsTrigger>
           </TabsList>
 
-        <div className="min-h-[70vh] w-full">
-          <TabsContent value="educación" className="w-full">
-            educación
-          </TabsContent>
-        </div>
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="educación" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="text-base max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.desc}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-1 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] w-[600px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <div>
+                            <span className="text-xl max-w-[360px] pb-2 text-center lg:text-left">
+                              {item.position}
+                            </span>
+                            <div className="flex items-center gap-3">
+                              <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                              <h3 className="text-white/60">
+                                {item.institución}
+                              </h3>
+                            </div>
+                            <p className="text-accent">{item.duration}</p>
+                          </div>
+                          <div>
+                            <Link
+                              href="https://drive.google.com/file/d/14GnVGFB-UY2tOT0ih-5Qfyns_DPeL8lx/view?usp=sharing"
+                              legacyBehavior
+                              passHref
+                            >
+                              <a target="_blank" rel="noopener noreferrer">
+                                <h1 className="font-thin text-white/70 hover:text-accent transition-all duration-500">
+                                  {item.certi}
+                                </h1>
+                              </a>
+                            </Link>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="habilidades" className="w-full">
+              mis habilidades
+            </TabsContent>
+            <TabsContent value="sobremi" className="w-full">
+              sobre mi
+            </TabsContent>
+          </div>
         </Tabs>
       </div>
     </motion.div>
