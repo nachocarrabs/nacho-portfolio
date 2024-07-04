3ba42378f4008f0f7d869d4af7f48f23200b4fbe
ignacio <ignaciocarrabs@gmail.com>
2024-07-03 21:19:06 -0300

    cambios resumen

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -115,6 +115,10 @@ const skills = {
       name: "Express.js"
     },
     {
+      icon: <SiNextdotjs />,
+      name: "Next.js",
+    },
+    {
       icon: <FaJava />,
       name: "Java",
     },
@@ -137,10 +141,6 @@ const skills = {
     {
       icon: <SiAdobeillustrator />,
       name: "Illustrator",
-    },
-    {
-      icon: <SiNextdotjs />,
-      name: "Next.js",
     },
     {
       icon: <SiSass/>,
@@ -228,13 +228,13 @@ const Resume = () => {
                   <h3 className="text-4xl font-bold ">{skills.title}</h3>
                   <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
                 </div>
-                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4">
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[20px] gap-3">
                   {skills.skillList.map((skill, index) =>{
                     return <li key={index}>
                       <TooltipProvider delayDuration={100}>
                         <Tooltip>
-                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
-                          <div className="text-6xl hover:text-accent transition-all duration-300">{skill.icon}</div>
+                          <TooltipTrigger className="w-full h-[100px] bg-[#232329] rounded-xl flex justify-center items-center group hover:border  hover:border-accent transition-all duration-300">
+                          <div className="text-4xl hover:text-accent transition-all duration-300">{skill.icon}</div>
                           </TooltipTrigger>
                           <TooltipContent>
                             <p className="capitalize">{skill.name}</p>
